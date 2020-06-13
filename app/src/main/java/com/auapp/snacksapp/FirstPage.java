b0ec225644c86583f498ff67ada5ff05cf575e1b
Abhijeet Shinde13 <33342683+abhijeet-shinde264@users.noreply.github.com>
2020-06-13 22:05:53 +0530

    Third Commit

--- app/src/main/java/com/auapp/snacksapp/FirstPage.java
+++ app/src/main/java/com/auapp/snacksapp/FirstPage.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -40,14 +41,15 @@ import id.zelory.compressor.Compressor;
 
 public class FirstPage extends AppCompatActivity {
     private ImageView img;
-    private EditText itname,quantity;
+    private EditText itname;
     Button add;
+    TextView quan;
     private Bitmap compressedImageFile=null;
     private Uri imgUrl = null;
 
     private StorageReference mStorageRef;
     private DatabaseReference mDatabaseRef;
-
+    int minteger = 0;
     private StorageTask mUploadTask;
     private static final int PICK_IMAGE_REQUEST = 1;
 
@@ -59,7 +61,7 @@ public class FirstPage extends AppCompatActivity {
         setContentView(R.layout.activity_first_page);
         img = findViewById(R.id.addimage);
         itname = findViewById(R.id.item_name);
-        quantity = findViewById(R.id.quantit);
+        quan = findViewById(R.id.integer_number);
         add = findViewById(R.id.additem);
         uploadProgress = findViewById(R.id.uploadProgress);
         mStorageRef = FirebaseStorage.getInstance().getReference().child("Items");
@@ -86,51 +88,47 @@ public class FirstPage extends AppCompatActivity {
                 }
             }
         });
+    }public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+        if (minteger>10){
+            Toast.makeText(this, "Sorry u cant increment more", Toast.LENGTH_SHORT).show();
+            display(0);
+        }
+    }public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        display(minteger);
+        if (minteger<0){
+            Toast.makeText(this, "Sorry u cant decrement more", Toast.LENGTH_SHORT).show();
+            display(0);
+        }
     }
-
+    private void display(int number) {
+        quan.setText("" + number);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-//            imgUrl = data.getData();
-//
-//            Picasso.with(this).load(imgUrl).into(imgPreview);
-//        }
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-
                 imgUrl = result.getUri();
                 Picasso.with(this).load(imgUrl).networkPolicy(NetworkPolicy.OFFLINE).into(img);
-
                 File crop_path=new File(imgUrl.getPath());
-
-
                 try {
                     compressedImageFile=new Compressor(this)
                             .setMaxHeight(300)
                             .setMaxWidth(300).setQuality(100).compressToBitmap(crop_path);
                 }
-
                 catch (IOException e)
                 {
                     e.printStackTrace();
                 }
-
-
-
-
-
-
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
 
                 Exception error = result.getError();
-
-
             }
         }
-
-
     }
 
     private String getFileExtension(Uri uri) {
@@ -157,13 +155,15 @@ public class FirstPage extends AppCompatActivity {
                             fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                                 @Override
                                 public void onSuccess(Uri uri) {
-                                    Upload upload = new Upload(itname.getText().toString().trim(),quantity.getText().toString().trim(),uri.toString());
+                                    Upload upload = new Upload(itname.getText().toString().trim(),quan.getText().toString().trim(),uri.toString());
                                     String uploadID = mDatabaseRef.push().getKey();
                                     mDatabaseRef.child(uploadID).setValue(upload);
                                     Toast.makeText(FirstPage.this, "Upload successfully", Toast.LENGTH_LONG).show();
                                     img.setImageResource(R.drawable.imagepreview);
                                     itname.setText("");
-                                    quantity.setText("");
+                                    quan.setText("");
+                                    Intent i1 = new Intent(FirstPage.this,ShowSnacks.class);
+                                    startActivity(i1);
                                 }
                             });
 
--- app/src/main/java/com/auapp/snacksapp/ShowSnacks.java
+++ app/src/main/java/com/auapp/snacksapp/ShowSnacks.java
@@ -14,8 +14,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -38,10 +41,14 @@ public class ShowSnacks extends AppCompatActivity{
 
     private DatabaseReference mDatabaseRef;
     private List<Upload> mUploads;
-    FloatingActionButton fb;
+   // FloatingActionButton fb;
     FirebaseAuth mauth;
     String currentuserid;
     String email1;
+    TextView up,del,add;
+    private FloatingActionButton fab_main, fab1_update, fab2_delete,fab3_add;
+    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
+    Boolean isOpen = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,7 +63,70 @@ public class ShowSnacks extends AppCompatActivity{
         //mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         progressBar = findViewById(R.id.progress_circular);
-        fb = findViewById(R.id.floatingActionButton);
+        //fb = findViewById(R.id.floatingActionButton);
+        fab_main = findViewById(R.id.fab);
+        fab1_update = findViewById(R.id.fab1_up);
+        fab2_delete = findViewById(R.id.fab2_del);
+        fab3_add = findViewById(R.id.fab3_addddd);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_clock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_clock);
+        fab_anticlock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_anticlock);
+        up = findViewById(R.id.update);
+        del = findViewById(R.id.delete);
+        add = findViewById(R.id.addaa);
+        fab_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                    up.setVisibility(View.INVISIBLE);
+                    del.setVisibility(View.INVISIBLE);
+                    add.setVisibility(View.INVISIBLE);
+                    fab2_delete.setAnimation(fab_close);
+                    fab1_update.setAnimation(fab_close);
+                    fab3_add.setAnimation(fab_close);
+                    fab_main.setAnimation(fab_anticlock);
+                    fab2_delete.setClickable(false);
+                    fab1_update.setClickable(false);
+                    fab3_add.setClickable(false);
+                    isOpen = false;
+                }else {
+                    up.setVisibility(View.VISIBLE);
+                    del.setVisibility(View.VISIBLE);
+                    add.setVisibility(View.VISIBLE);
+                    fab2_delete.startAnimation(fab_open);
+                    fab1_update.startAnimation(fab_open);
+                    fab3_add.startAnimation(fab_open);
+                    fab_main.startAnimation(fab_clock);
+                    fab2_delete.setClickable(true);
+                    fab1_update.setClickable(true);
+                    fab3_add.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+        fab2_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //openDialogDelete();
+                //Toast.makeText(getApplicationContext(), "Delete", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Page Under Development", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fab1_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Page Under Development", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        fab3_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setTitle("Show Snacks");
@@ -93,12 +163,38 @@ public class ShowSnacks extends AppCompatActivity{
 //                        }
 //                    });
 //                }
-        fb.setOnClickListener(new View.OnClickListener() {
+//        fb.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openDialog();
+//            }
+//        });
+    }
+
+    private void openDialogDelete() {
+        View view = LayoutInflater.from(ShowSnacks.this).inflate(R.layout.dialog_show,null);
+        final EditText email = view.findViewById(R.id.email111);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ShowSnacks.this);
+        builder.setMessage("Please enter your email")
+                .setView(view)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String em = email.getText().toString().trim();
+                        if (em.equals("abhijeetshinde1999@gmail.com")){
+                            startActivity(new Intent(ShowSnacks.this,DeletePage.class));
+                        }else{
+                            Toast.makeText(ShowSnacks.this, "Sorry your not admin", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openDialog();
+            public void onClick(DialogInterface dialog, int which) {
+                //Toast.makeText(ShowSnacks.this, "Sorry your not admin", Toast.LENGTH_SHORT).show();
             }
         });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     private void openDialog() {
