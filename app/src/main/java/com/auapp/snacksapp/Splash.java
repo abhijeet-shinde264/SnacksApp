32c7152124a2ac91e75a1b2ff94c8bbfc37ddf8e
Abhijeet Shinde13 <33342683+abhijeet-shinde264@users.noreply.github.com>
2020-06-15 20:17:50 +0530

    15/06/2020 Commit

---
+++ app/src/main/java/com/auapp/snacksapp/Splash.java
@@ -0,0 +1,47 @@
+package com.auapp.snacksapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Splash extends AppCompatActivity {
+
+    ImageView im;
+    TextView t1;
+    Animation topAnim,bottomAnim;
+    private FirebaseAuth mAuth;
+    public FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mAuth = FirebaseAuth.getInstance();
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        im = findViewById(R.id.imageView);
+        t1 = findViewById(R.id.textView);
+//Set animation to elements
+        im.setAnimation(topAnim);
+        t1.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i1 = new Intent(Splash.this,MainActivity.class);
+                startActivity(i1);
+                finish();
+            }
+        },1500);
+    }
+}
